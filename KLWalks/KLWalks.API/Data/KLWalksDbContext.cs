56671259c0b55bfd608319f3eb8200de3649cd74
devika <devikamurali08@gmail.com>
2023-01-16 07:26:15 +0530

    End of Section1

---
+++ KLWalks/KLWalks.API/Data/KLWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using KLWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace KLWalks.API.Data
+{
+    public class KLWalksDbContext : DbContext
+    {
+        public KLWalksDbContext(DbContextOptions<KLWalksDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ KLWalks/KLWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace KLWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
